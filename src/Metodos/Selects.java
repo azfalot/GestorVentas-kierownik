67eb9d147fc61ff814da395fa5d4821f0657be77
azfalot <cj_driking@hotmail.com>
2014-06-29 20:37:04 +0200

    Interfaz terminada
    Limando algunos detalles de la aplicacion
    Insertada opcion DELETE en todos los ambitos
    
    Introduccion del sistema de ayuda [50%]
    Falta introduccir la ayuda en documentos HTMLy esta hecho
    
    Iniciando con Informes

--- src/Metodos/Selects.java
+++ src/Metodos/Selects.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Metodos;
 
 /**
@@ -11,48 +10,49 @@ package Metodos;
  * @author carlos
  */
 public class Selects {
-    
-    /*
-    Aqui iran todas las consultas que en algun momento se puedan ejecutar a 
-    traves de alguna accion
-    
-    */
-    
-   private final String rellenarPedidos;
-   private final String rellenarClientes;
-   private final String rellenarProductos;
-   private final String nombresDeClientes;
-   private final String nombreDeProductos;
-   private final String obtenerFecha;
 
+    /*
+     Aqui iran todas las consultas que en algun momento se puedan ejecutar a 
+     traves de alguna accion
+    
+     */
+    private final String rellenarPedidos;
+    private final String rellenarClientes;
+    private final String rellenarProductos;
+    private final String nombresDeClientes;
+    private final String nombreDeProductos;
+    private final String obtenerFecha;
+    private final String allFromProveedores;
 
     public Selects() {
-        this.obtenerFecha="SELECT date('now');";
-        this.nombreDeProductos="SELECT nombre FROM Productos";
-        this.nombresDeClientes="SELECT nombre FROM clientes";
-        this.rellenarProductos="SELECT nombre, pvp, stock FROM productos;";
-        this.rellenarClientes ="SELECT nombre, dni, pais, email from clientes;";
-        this.rellenarPedidos ="SELECT p.idpedido, c.nombre, p.fecha, sum(pr.pvp*dp.cantidad)total, p.estado\n" +
-                              "FROM productos pr, pedidos p, clientes c, detallepedidos dp\n" +
-                              "WHERE dp.fk_producto=pr.idproducto and dp.fk_pedido=p.idpedido and p.fk_cliente=c.idcliente\n" +
-                              "GROUP BY p.idpedido";
+        this.obtenerFecha = "SELECT date('now');";
+        this.nombreDeProductos = "SELECT nombre FROM Productos";
+        this.allFromProveedores = "SELECT * FROM Proveedores";
+        this.nombresDeClientes = "SELECT nombre FROM clientes";
+        this.rellenarProductos = "SELECT nombre, pvp, stock FROM productos;";
+        this.rellenarClientes = "SELECT nombre, dni, pais, email from clientes;";
+        this.rellenarPedidos = "SELECT p.idpedido, c.nombre, p.fecha, sum(pr.pvp*dp.cantidad)total, p.estado\n"
+                + "FROM productos pr, pedidos p, clientes c, detallepedidos dp\n"
+                + "WHERE dp.fk_producto=pr.idproducto and dp.fk_pedido=p.idpedido and p.fk_cliente=c.idcliente\n"
+                + "GROUP BY p.idpedido";
+    }
+
+    public String getAllFromProveedores() {
+        return allFromProveedores;
     }
 
     public String getObtenerFecha() {
         return obtenerFecha;
     }
-    
 
     public String getNombreDeProductos() {
         return nombreDeProductos;
     }
 
-    
     public String getNombresDeClientes() {
         return nombresDeClientes;
     }
 
-    
     public String getRellenarPedidos() {
         return rellenarPedidos;
     }
@@ -60,55 +60,63 @@ public class Selects {
     public String getRellenarClientes() {
         return rellenarClientes;
     }
-    
+
     // SELECT CLIENTES POR NOMBRE
-    public String clientePorNombre(String n){
-        
-        String consulta = "SELECT nombre, dni, pais, email from clientes "+
-                          "WHERE nombre='"+n+"';";
+    public String clientePorNombre(String n) {
+
+        String consulta = "SELECT nombre, dni, pais, email from clientes "
+                + "WHERE nombre='" + n + "';";
         return consulta;
     }
+
     //SELECT PEDIDOS POR NOMBRE
-    public String pedidosPorNombre(String n){
-        String consulta = "SELECT p.idpedido, c.nombre, p.fecha, sum(pr.pvp*dp.cantidad)total, p.estado\n" +
-                          "FROM productos pr, pedidos p, clientes c, detallepedidos dp\n" +
-                          "WHERE dp.fk_producto=pr.idproducto and dp.fk_pedido=p.idpedido and p.fk_cliente=c.idcliente and c.nombre='"+n+"'\n" +
-                          "GROUP BY p.idpedido;";
+    public String pedidosPorNombre(String n) {
+        String consulta = "SELECT p.idpedido, c.nombre, p.fecha, sum(pr.pvp*dp.cantidad)total, p.estado\n"
+                + "FROM productos pr, pedidos p, clientes c, detallepedidos dp\n"
+                + "WHERE dp.fk_producto=pr.idproducto and dp.fk_pedido=p.idpedido and p.fk_cliente=c.idcliente and c.nombre='" + n + "'\n"
+                + "GROUP BY p.idpedido;";
         return consulta;
     }
+
     //SELECT ID CLIENTE POR NOMBRE
-    public String idClientePorNomrbe(String n){
-        String consulta = "SELECT idCliente \n" +
-                          "FROM Clientes\n" +
-                          "WHERE nombre='"+n+"';";
+    public String idClientePorNomrbe(String n) {
+        String consulta = "SELECT idCliente \n"
+                + "FROM Clientes\n"
+                + "WHERE nombre='" + n + "';";
         return consulta;
     }
+
     //SELECT ID PRODUCTO POR NOMBRE
-    public String idProductoPorNombre(String n){
-        String consulta = "SELECT idProducto \n" +
-                          "FROM Productos\n" +
-                          "WHERE nombre='"+n+"';";
+    public String idProductoPorNombre(String n) {
+        String consulta = "SELECT idProducto \n"
+                + "FROM Productos\n"
+                + "WHERE nombre='" + n + "';";
         return consulta;
     }
+
+    //SELECT STOCK PRODUCTO POR ID
+    public String stockProductoPorId(int id) {
+
+        String consulta = "SELECT stock \n"
+                + "FROM Productos\n"
+                + "WHERE idProducto=" + id + ";";
+        return consulta;
+
+    }
+
     //Obtener el precio de un producto
-    public String getPrecioPorNombre(String d){
-        
-        String consulta = "SELECT pvp \n" +
-                          "FROM Productos\n" +
-                          "WHERE nombre='"+d+"';";
-        
-       return consulta;
-        
-     
+    public String getPrecioPorNombre(String d) {
+
+        String consulta = "SELECT pvp \n"
+                + "FROM Productos\n"
+                + "WHERE nombre='" + d + "';";
+
+        return consulta;
+
     }
 
     public String getRellenarProductos() {
         return rellenarProductos;
     }
-    
-    
 
-    
-    
-    
 }
